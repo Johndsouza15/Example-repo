76001e1fe31012bcbd4c12c5645f00801fd80771
John Dsouza <dsouza.pjohn15@gmail.com>
2025-11-14 23:14:39 +0530

    add book file

---
+++ book.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class book_id
+{
+	public static void main(String args [])
+	{
+		Scanner s =new Scanner(System.in);
+		 int number=s.nextInt();
+		System.out.println("No:"+number);
+	}
+}
+
+		
